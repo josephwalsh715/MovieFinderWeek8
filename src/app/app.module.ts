ee4c988be18fc10dc80c581264a3c04f7d2a219f
Your Namejosephwalsh715 <josephwalsh715@gmail.com>
2023-03-15 12:16:30 +0000

    added searchtitle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,38 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
 import { OmdbApiService } from './services/omdb-api.service';
 import { SearchtitleComponent } from './components/searchtitle/searchtitle.component';
+
+import { AboutComponent } from './components/about/about.component';
+import { SearchComponent } from './components/search/search.component';
+
+
+
+
+
+const routes:Routes= [
+  {path:'', component:SearchtitleComponent},
+  {path:'about', component:AboutComponent},
+  {path:'search', component:SearchComponent},
+]
+
+imports:[
+  BrowserModule,
+  RouterModule.forRoot(routes),
+  HttpClientModule
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchtitleComponent
+    SearchtitleComponent,
+    AboutComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/searchtitle/searchtitle.component.ts
+++ src/app/components/searchtitle/searchtitle.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { OmdbApiService } from 'src/app/services/omdb-api.service';
+import { IOMDBResponse } from 'src/app/omdbresponse';
 
 @Component({
   selector: 'app-searchtitle',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class SearchtitleComponent {
 
+constructor(private _omdbService:OmdbApiService){}
+
+  title = 'Movie Finder';
+  movieData:IOMDBResponse | undefined;
+  errorMessge:any;
+  name = 'Joe Walsh';
+  studentNo = 'S00222915';
+
+  
+
+  getMovieDetails(movieName:string): boolean {
+    this._omdbService.getMovieData(movieName).subscribe(
+      movieData => {
+        this.movieData=movieData;
+        console.log("Director name : " + this.movieData.Director)
+      }
+    )
+    return false;
+  }
 }
+
+
