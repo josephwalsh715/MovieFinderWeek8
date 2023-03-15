2f1faa2e485bd677a0b1812fcb68de601e829eb8
Your Namejosephwalsh715 <josephwalsh715@gmail.com>
2023-03-15 11:07:24 +0000

    added searchtitle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { OmdbApiService } from './services/omdb-api.service';
+import { SearchtitleComponent } from './components/searchtitle/searchtitle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchtitleComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/searchtitle/searchtitle.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-searchtitle',
+  templateUrl: './searchtitle.component.html',
+  styleUrls: ['./searchtitle.component.css']
+})
+export class SearchtitleComponent {
+
+}
